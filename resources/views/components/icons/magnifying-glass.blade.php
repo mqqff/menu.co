c733c3ae01c60e042201dd43aebd0280be1e5d09
Atha Tsaqif <athatsaaqif@gmail.com>
2026-04-02 10:36:47 +0700

    feat: replace SVG icons with Blade components

---
+++ resources/views/components/icons/magnifying-glass.blade.php
@@ -0,0 +1,3 @@
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" {{ $attributes }}>
+    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-4.35-4.35M17 11A6 6 0 1 1 5 11a6 6 0 0 1 12 0z" />
+</svg>
