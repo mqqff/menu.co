2afb83e4e4e0429aa31bc815039e17dbd1f0868f
Atha Tsaqif <athatsaaqif@gmail.com>
2026-04-28 09:45:05 +0700

    feat: add cursor pointer style to password visibility toggle icon

--- resources/views/components/auth/input-field.blade.php
+++ resources/views/components/auth/input-field.blade.php
@@ -44,7 +44,7 @@
                        hover:text-gray-600 transition-colors focus:outline-none"
                 aria-label="Toggle password visibility">
                 <svg id="eye-icon-{{ $name }}" xmlns="http://www.w3.org/2000/svg"
-                     class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                     class="h-5 w-5 cursor-pointer" fill="none" viewBox="0 0 24 24"
                      stroke="currentColor" stroke-width="1.8">
                     <path stroke-linecap="round" stroke-linejoin="round"
                           d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
