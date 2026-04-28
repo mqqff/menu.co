02b12b8ae3c9b7ada25a7b7018fd569b20f17cbf
Atha Tsaqif <athatsaaqif@gmail.com>
2026-04-28 09:45:17 +0700

    feat: improve styling for success and error messages in login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,13 +45,13 @@
                     @csrf
 
                     @if (session('success'))
-                        <div class="bg-white border border-green-300 text-green-600 font-medium text-center text-sm rounded-md px-4 py-2">
+                        <div class="bg-green-300 border border-green-400/60 text-green-800 font-medium text-center text-sm rounded-md px-4 py-2">
                             {{ session('success') }}
                         </div>
                     @endif
 
                     @if (session('error'))
-                        <div class="bg-white border border-red-300 text-red-600 font-medium text-center text-sm rounded-md px-4 py-2">
+                        <div class="bg-red-300 border border-red-400/60 text-red-800 font-medium text-center text-sm rounded-md px-4 py-2">
                             {{ session('error') }}
                         </div>
                     @endif
